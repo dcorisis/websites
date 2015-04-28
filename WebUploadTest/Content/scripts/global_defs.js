524f82c36efa5a5dcf7e239abc2143b56436d582
dcorisis <djcorisis@gmail.com>
2015-04-27 19:50:25 -0700

    added global header

---
+++ WebUploadTest/Content/scripts/global_defs.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    //loadHeaders();
+});
+
+function loadHeaders(isHome)
+{
+    if(isHome)
+        $("#header").load("./views/Header.html");
+    else
+        $("#header").load("./Header.html");
+}
